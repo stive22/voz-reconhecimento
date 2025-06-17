9544864027860661af1900916d0d6c854e317571
stive22 <fernandoviana_guedes@yahoo.com.br>
2025-06-17 00:03:50 -0300

    Update reconhecimento.py

--- reconhecimento.py
+++ reconhecimento.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 def extrair_mfcc(caminho_audio):
-    y, sr = librosa.load(caminho_audio, sr=16000)
+    y, sr = librosa.load(caminho_audio, sr=48000)
     mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
     return np.mean(mfcc, axis=1)
 
@@ -19,6 +19,6 @@ def comparar_vozes(arquivo_verificacao):
 
     if distancias:
         usuario, distancia = min(distancias.items(), key=lambda x: x[1])
-        if distancia < 50:  # limiar ajustável
+        if distancia < 50:
             return usuario
     return None
